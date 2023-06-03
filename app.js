12caac3c4568d21b6e81c20bccf40a51ea063202
saulocmd <saullochristian@outlook.com>
2023-06-03 20:29:46 -0300

    voltando para edição anterior

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao bancio de dados")
-//mais um console log de teste:
-console.log("Saulo commitou")
-console.log("Enviando para o github")
